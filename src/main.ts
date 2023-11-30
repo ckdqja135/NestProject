b80d579a5cb53b202d8953ce223611f14f175691
cbsong <ckdqja135@naver.com>
2023-11-30 17:08:33 +0900

    feat: 스웨거수정

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3095;
+  const port = process.env.PORT || 3030;
   await app.listen(port);
   console.log(`listening on port ${port}`);
 
--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -2,19 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class JoinRequestDto {
   @ApiProperty({
-    example: 'zerohch0@gmail.com',
+    example: 'ckdqja135@gmail.com',
     description: '이메일',
   })
   public email: string;
 
   @ApiProperty({
-    example: '제로초',
+    example: 'changbami',
     description: '닉네임',
   })
   public nickname: string;
 
   @ApiProperty({
-    example: 'nodejsbook',
+    example: '1234',
     description: '비밀번호',
   })
   public password: string;
